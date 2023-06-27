4ab5039087004799e159167542b166228832c7e5
srkim <srkim@DESKTOP-ED02VVS>
2023-06-27 15:40:28 +0900

    FollowDTO 생성 followRequest / followList에 follow 테이블 내용 가지고오기

---
+++ src/main/java/com/friends/dto/FollowDTO.java
@@ -0,0 +1,17 @@
+package com.friends.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@Setter
+@ToString
+@Slf4j
+public class FollowDTO {
+	
+	String userName;
+	String follow;
+
+}
